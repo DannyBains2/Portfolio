4cabecf8114a825b05c40638573545b141a28452
dannybains2 <dannybains2@gmail.com>
2023-08-05 22:52:55 +0100

    working on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,33 +11,38 @@ function Navbar() {
         logo
         </div>
         <div onClick={()=>setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
-          {open===false? <HiBars3/>: <HiXMark/>}
+          {open? <HiXMark/> :<HiBars3/>}
         {/* <HiBars3 name={open ? "close":"menu"}/> */}
 
         </div>
-          <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? "top-20 opacity-20": "top-[-490px]"} md:opacity-100 opacity-0`}>
-            <li className="md:ml-8 text-xl md:my-0 my-7 text-gray-800 hover:text-gray-400 hover:bg-slate-600 duration-500 cursor-pointer">
+          <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? "top-20 ": "top-[-490px]"} `}>
+            <li className="md:ml-8 text-xl md:my-0 my-7 text-gray-800 hover:text-gray-400 hover:bg-slate-600 duration-500 cursor-pointer rounded-md pl-3">
               <>
                 Home
               </>
             </li>
-            <li className="md:ml-8 text-xl md:my-0 my-7 text-gray-800 hover:text-gray-400  hover:bg-slate-600 duration-500 cursor-pointer">
+            <li className="md:ml-8 text-xl md:my-0 my-7 text-gray-800 hover:text-gray-400  hover:bg-slate-600 duration-500 cursor-pointer rounded-md pl-3">
               <>
                 Portfolio
               </>
             </li>
-            <li className="md:ml-8 text-xl md:my-0 my-7 text-gray-800 hover:text-gray-400 hover:bg-slate-600 duration-500 cursor-pointer">
+            <li className="md:ml-8 text-xl md:my-0 my-7 text-gray-800 hover:text-gray-400 hover:bg-slate-600 duration-500 cursor-pointer rounded-md pl-3">
               <>
               About me
               </>
             </li>
-            <li className="md:ml-8 text-xl md:my-0 my-7 text-gray-800 hover:text-gray-400   hover:bg-slate-600 duration-500 cursor-pointer">
+            <li className="md:ml-8 text-xl md:my-0 my-7 text-gray-800 hover:text-gray-400   hover:bg-slate-600 duration-500 cursor-pointer rounded-md pl-3">
               <>
               Contact Me
               </>
-            </li>
-            <Button>Git Hub</Button>
-            <Button>LinkedIn</Button>
+            </li >
+            <div className="space-x-2">
+            
+              <Button >Git Hub</Button>
+             
+              <Button>LinkedIn</Button>
+            </div>
+            
           </ul>
       </div>
     </div>
